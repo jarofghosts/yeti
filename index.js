f354a5f3bb1d5ffeee2684157189dff7671a786a
jesse keane <me@jessekeane.me>
2013-08-06 00:22:18 +0400

    user db

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var nano = require('nano'),
-    router = require('route-emitter').createRouter();
-
-
+var nano = require('nano')('http://localhost:5984'),
+    router = require('route-emitter').createRouter(),
+    db = nano.db.use('yeti'),
+    users = require('./lib/user.js')(db);
