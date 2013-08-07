e76fa276ff2702490fa095dc846b7d30b154b8d8
jesse keane <me@jessekeane.me>
2013-08-07 09:03:22 +0400

    errr maybe

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var nano = require('nano')('http://localhost:5984'),
     restify = require('restify'),
+    server = restify.createServer(),
     db = nano.db.use('yeti'),
     users = require('./lib/user.js')(db);
 
+server.use(restify.bodyParser());
+server.use(restify.queryParser());
+server.use(restify.gzipResponse());
+server.use(restify.authorizationParser());
+
+server.get('/user', users.getUser);
+server.post('/user', users.insertUser);
 
