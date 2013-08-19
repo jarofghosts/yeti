67fa108ae32e183970d76c4b63e8b2134ead662f
jesse keane <me@jessekeane.me>
2013-08-19 04:12:26 +0400

    slow and steady wins the race

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var nano = require('nano')('http://localhost:5984'),
     restify = require('restify'),
     server = restify.createServer(),
     db = nano.db.use('yeti'),
-    users = require('./lib/user.js')(db);
+    users = require('./lib/user.js')(db),
+    auth = require('./lib/auth.js')(db, restify);
 
 server.use(restify.bodyParser());
 server.use(restify.queryParser());
@@ -11,4 +12,5 @@ server.use(restify.authorizationParser());
 
 server.get('/user', users.getUser);
 server.post('/user', users.insertUser);
+server.put('/login', auth.processLogin);
 
