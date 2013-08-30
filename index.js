272cfb4435931da451a0fe58dfde8efeaa42fe4a
jesse keane <me@jessekeane.me>
2013-08-30 08:38:13 +0400

    cleanup

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var nano = require('nano')('http://localhost:5984'),
     db = nano.db.use('yeti'),
+    cookies = require('cookies'),
     router = require('route-emitter').createRouter(),
     parseParams = require('http-params').parse;
 
+router.listen('get', '/user', 
