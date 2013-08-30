e7182ae15f0c88ae52236da857c3ba8d06c37c22
jesse keane <me@jessekeane.me>
2013-08-30 07:22:45 +0400

    SHUT IT DOWN

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 var nano = require('nano')('http://localhost:5984'),
-    restify = require('restify'),
-    server = restify.createServer(),
     db = nano.db.use('yeti'),
-    users = require('./lib/user.js')(db),
-    auth = require('./lib/auth.js')(db, restify);
-
-server.use(restify.bodyParser());
-server.use(restify.queryParser());
-server.use(restify.gzipResponse());
-server.use(restify.authorizationParser());
-
-server.get('/user', users.getUser);
-server.post('/user', users.insertUser);
-server.put('/login', auth.processLogin);
+    router = require('route-emitter').createRouter(),
+    parseParams = require('http-params').parse;
 
