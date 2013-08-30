82cf071ccfead0ac4817b16f7691ae73cdf42080
jesse keane <me@jessekeane.me>
2013-08-30 08:46:48 +0400

    routing

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var nano = require('nano')('http://localhost:5984'),
     Session = require('redis-sessions'),
     rs = new Session(),
     router = require('route-emitter').createRouter(),
-    parseParams = require('http-params').parse;
+    parseParams = require('http-params').parse,
+    routes = {
+      users: require('./routes/user.js'),
+      posts: require('./routes/post.js')
+    };
 
-router.listen('get', '/user', 
+router.listen('get', '/user', users.getUser);
+router.listen('post', '/user', users.postUser);
+router.listen('put', '/user', users.putUser);
+router.listen('delete', '/user', users.deleteUser);
+
