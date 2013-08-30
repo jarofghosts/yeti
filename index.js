428b46519436f363e8415793adb4931bd0266356
jesse keane <me@jessekeane.me>
2013-08-30 08:41:19 +0400

    use redis sessions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var nano = require('nano')('http://localhost:5984'),
     db = nano.db.use('yeti'),
     cookies = require('cookies'),
+    Session = require('redis-sessions'),
+    rs = new Session(),
     router = require('route-emitter').createRouter(),
     parseParams = require('http-params').parse;
 
