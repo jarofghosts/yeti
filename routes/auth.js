4939caa069adc2798200193575bf6618e8fbd047
jesse keane <me@jessekeane.me>
2013-09-14 04:30:14 +0400

    proper formatting

--- routes/auth.js
+++ routes/auth.js
@@ -8,22 +8,22 @@ module.exports = function (yeti) {
     },
 
     putLogin: function (req, res) {
-      if (!req.params.username || !req.params.password) return yeti.sendJson(403, new yeti.Error('Send credetials')); 
+      if (!req.params.username || !req.params.password) return yeti.sendJson(res, 403, new yeti.Error('Send credetials')); 
       db.view('users', 'password_by_username',
         { keys: [req.params.username] },
         function (err, body) {
-          if (err) return yeti.sendJson(500, new yeti.Error('Database error'));
+          if (err) return yeti.sendJson(res, 500, new yeti.Error('Database error'));
 
-          if (!body.rows.length) return yeti.sendJson(404, new yeti.Error('No such user')); 
+          if (!body.rows.length) return yeti.sendJson(res, 404, new yeti.Error('No such user')); 
           
           bcrypt.compare(req.params.password,
             body.rows[0].value,
             function (err, matches) {
-              if (!matches) return yeti.sendJson(403, new yeti.Error('Authentication failed')); 
+              if (!matches) return yeti.sendJson(res, 403, new yeti.Error('Authentication failed')); 
               rs.create({ app: 'yeti', id: req.params.username },
                 function (err, resp) {
-                  if (err) return yeti.sendJson(500, new yeti.Error('Session error')); 
-                  yeti.sendJson(new yeti.Response({ token: resp.token }));
+                  if (err) return yeti.sendJson(res, 500, new yeti.Error('Session error')); 
+                  yeti.sendJson(res, new yeti.Response({ token: resp.token }));
                 });
             });
         });
@@ -32,11 +32,8 @@ module.exports = function (yeti) {
     putLogout: function (req, res) {
       rs.kill({ app: 'yeti', token: req.session.token },
         function (err, resp) {
-          if (err || !resp || !resp.kill) {
-            res.writeHead(500);
-            return res.end('Bad');
-          };
-          return res.end(JSON.stringify({ success: true }));
+          if (err || !resp || !resp.kill) return yeti.sendJson(res, 500, new yeti.Error('Session error'));
+          yeti.sendJson(res, { loggedOut: true });
         });
     }
 
